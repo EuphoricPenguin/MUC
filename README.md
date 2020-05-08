ff17e1defdbd0e23bb1f1ea0918c10af68c975d1
EuphoricPenguin <euphoricpenguin@protonmail.com>
2020-05-07 21:43:51 -0400

    1.3.0 - Added basic embed support

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ If you are looking to use this bot in your own server, a public version will be 
 
 MUC (Markov User Cloner) is a new Discord bot project of mine; it will be regularly updated, and is intended to be a public bot. While many bots exist to facilitate interaction with markov chains, I wish to create a simple open source example that people can both interact with and modify. This serves mainly as a refresher for me to try some new things in JavaScript, and as my first foray into public bot development.               
 
-With v1.2.x, three commands are present:
+With v1.3.x, three commands are present:
 
 `,m clone @user` will clone a user based on their id, or the keyword `all` (no @).
 The bot uses the last 100 sent messages to generate a markov chain, filtering based on id.
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Markov User Cloner (MUC or M.U.C)
  * (c) EuphoricPenguin, MIT License
- * v1.2.0
+ * v1.3.0
  */
 require("dotenv").config();
 const config = require("./config.json");
@@ -26,7 +26,7 @@ client.on("message", msg => {
     let commandArr = command.split(" ").filter(i => i !== "");
 
     //Strips mentions down to user id's
-    if (commandArr[1].includes("<@!")) {
+    if (commandArr.length > 1 && commandArr[1].includes("<@!")) {
         commandArr[1] = commandArr[1].substring(3, commandArr[1].length - 1);
     }
 
@@ -35,39 +35,55 @@ client.on("message", msg => {
         "clone": async function (id) {
             fetchMessages(id)
                 .then(msgs => {
-                    if (invalidUserCheck(msgs) != undefined) return;
+                    if (invalidUserCheck(msgs)) return;
                     guildsObj[guild] = new Markov();
                     console.log(`cloning ${id} in ${guild}`);
                     msgs.forEach(msg => {
                         guildsObj[guild].update(msg);
                     });
                     if (id === "all") {
-                        msg.channel.send(`\`everyone\` might say:
-                            \`\`\`${guildsObj[guild].generate()}\`\`\``);
+                        let embed = new Discord.MessageEmbed()
+                            .setColor("#000000")
+                            .setAuthor("Everyone might say:", "https://raw.githubusercontent.com/EuphoricPenguin/MUC/master/media/MUC.png")
+                            .setDescription(`\`\`\`${guildsObj[guild].generate()}\`\`\``);
+                        msg.channel.send(embed);
                     } else {
-                        msg.channel.send(`\`${id}\` might say:
-                            \`\`\`${guildsObj[guild].generate()}\`\`\``);
+                        let embed = new Discord.MessageEmbed()
+                            .setColor("#000000")
+                            .setAuthor(`${id} might say:`, "https://raw.githubusercontent.com/EuphoricPenguin/MUC/master/media/MUC.png")
+                            .setDescription(`\`\`\`${guildsObj[guild].generate()}\`\`\``);
+                        msg.channel.send(embed);
                     }
                 });
         },
         "regen": async function () {
             if (guild in guildsObj) {
                 console.log(`regenerating in ${guild}`);
-                msg.channel.send(`They also might say:
-                    \`\`\`${guildsObj[guild].generate()}\`\`\``);
+                let embed = new Discord.MessageEmbed()
+                    .setColor("#FFFFFF")
+                    .setAuthor("They also might say:", "https://raw.githubusercontent.com/EuphoricPenguin/MUC/master/media/MUC.png")
+                    .setDescription(`\`\`\`${guildsObj[guild].generate()}\`\`\``);
+                msg.channel.send(embed);
             } else {
-                msg.reply(`use \`${config.prefix} regen\` only after using \`${config.prefix} clone.\``);
+                let embed = new Discord.MessageEmbed()
+                    .setColor("#F93A2F")
+                    .setAuthor("Error:")
+                    .setDescription(`use \`${config.prefix} regen\` only after using \`${config.prefix} clone.\``);
+                msg.reply(embed);
             }
         },
         "help": async function () {
-            msg.author.send(config.helpIntro +
-                `
+            let embed = new Discord.MessageEmbed()
+                .setColor("#0099E1")
+                .setAuthor(config.helpIntro)
+                .setDescription(`
 Use \`${config.prefix} clone @user\` to clone anyone, or use \`all\` instead (no @) to clone everyone.
 If you want to re-generate a new message, use \`${config.prefix} regen\`.
 *The bot only uses the last 100 messages or so sent in the server, so keep this in mind.*
 **Here's some fun facts about this bot:**
 *Uptime:* ***${await fetchUptime()}***
 *Since it was last started, this bot has saw active use in ${Object.keys(guildsObj).length} guild(s).*`);
+            msg.author.send(embed);
             msg.reply("I sent you a DM with usage information.");
         }
     }
@@ -104,11 +120,23 @@ If you want to re-generate a new message, use \`${config.prefix} regen\`.
     }
 
     function invalidUserCheck(msgs) {
-        if (msgs.length < 1) return msg.reply("looks like that failed for some reason. Please try a different user, or check your command for typos.");
+        if (msgs.length < 1) {
+            let embed = new Discord.MessageEmbed()
+                .setColor("#F93A2F")
+                .setAuthor("Error:")
+                .setDescription(`*Huston, we have an ID problem.* Looks like you might've had a typo, or the user hasn't sent any plain-text messages. You can always ask for \`${config.prefix} help\``);
+            msg.reply(embed);
+            return true;
+        }
     }
 
     if (!commandIntp(commandArr, false)) {
-        msg.reply(`Invalid command. You can always ask for \`${config.prefix} help\``);
+        let embed = new Discord.MessageEmbed()
+            .setColor("#F93A2F")
+            .setAuthor("Error:")
+            .setDescription(`*Huston, we have an invalid command.* You can always ask for \`${config.prefix} help\``);
+        msg.reply(embed);
+
     } else {
         commandIntp(commandArr, true);
     }
